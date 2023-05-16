b894e440a412874da31ecb5f64040163a6df2b60
Yogurt7v <egorov-2k@yandex.ru>
2023-05-16 20:34:43 +0300

    Полная хрень

---
+++ webpackconfig.js
@@ -0,0 +1,10 @@
+const path = require ('path');
+
+module.exports = {
+    mode: 
+   entry: path.resolve (__dirname , 'src/Index.jsx'),
+   output :  {
+    path: path.resolve(__dirname , 'dist'),
+    filename: indexedDB.js 
+   }
+};

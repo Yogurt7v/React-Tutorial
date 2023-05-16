9d802a175e4a42fdae3135fec9492d32ad6e720d
Yogurt7v <egorov-2k@yandex.ru>
2023-05-16 19:39:19 +0300

    C & P

---
+++ Index.jsx
@@ -0,0 +1,11 @@
+import * as  React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Header } from './src/Header';
+
+window.addEventListener('load', () => {
+    ReactDOM.render(<Header />, document.getElementById('react_root'));
+})
+
+
+
+ 
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react' ;
+
+export function Header () {
+    return (
+        <header>
+            <h1>First message from React</h1>
+        </header>
+    )
+}
